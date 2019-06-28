0b141c70829da67dddfdb2a2f5db444610d863a7
Andreas Link <andreas.link@jamitlabs.com>
2019-06-28 08:57:54 +0200

    Update alamofire to version 5.0.0-beta.6

--- Package.swift
+++ Package.swift
@@ -3,13 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "Moya",
+    platforms: [.iOS("10.0"), .macOS("10.10"), .tvOS("10.0"), .watchOS("3.0")],
     products: [
         .library(name: "Moya", targets: ["Moya"]),
         .library(name: "ReactiveMoya", targets: ["ReactiveMoya"]),
         .library(name: "RxMoya", targets: ["RxMoya"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "4.1.0")),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .exact("4.8.2")),
         .package(url: "https://github.com/antitypical/Result.git", .upToNextMajor(from: "4.1.0")),
         .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", .upToNextMajor(from: ("5.0.0"))),
         .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "4.0.0"))
